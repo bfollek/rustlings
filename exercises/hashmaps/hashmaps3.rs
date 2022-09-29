cd2229cc34b574f19df2e6036ecf905e34ba2460
Bob Follek <bfollek@gmail.com>
2022-09-29 09:34:12 -0400

    HashMap done, but hashmaps3 needs work.

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -16,6 +16,14 @@
 
 // I AM NOT DONE
 
+/* TODO
+
+Move scores build into function.
+
+Handle team name without clone()?
+
+*/
+
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +31,16 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+impl Team {
+    fn new(name: String) -> Team {
+        Team {
+            name: name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        }
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -35,12 +53,27 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|team| team.goals_scored += team_1_score)
+            .and_modify(|team| team.goals_conceded += team_2_score)
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|team| team.goals_scored += team_2_score)
+            .and_modify(|team| team.goals_conceded += team_1_score)
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
+
     scores
 }
 
