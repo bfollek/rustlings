001f5a3959aad83e76f34ff9450935f2f6761aac
Bob Follek <bfollek@gmail.com>
2022-09-29 11:46:07 -0400

    HashMap.entry.and_modify takes a block for multiple actions.

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -71,8 +71,11 @@ fn update_team_scores(
 ) {
     scores
         .entry(name.clone())
-        .and_modify(|team| team.goals_scored += goals_scored)
-        .and_modify(|team| team.goals_conceded += goals_conceded)
+        .and_modify(|team| {
+            team.goals_scored += goals_scored;
+            team.goals_conceded += goals_conceded
+        })
+        //.and_modify(|team| team.goals_conceded += goals_conceded)
         .or_insert(Team {
             name,
             goals_scored,
