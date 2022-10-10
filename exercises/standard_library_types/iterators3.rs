03456fa864e7e09b4234667b325dd627420634e2
Bob Follek <bfollek@gmail.com>
2022-10-10 17:07:27 -0400

    exercises/standard_library_types/iterators3.rs

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -49,14 +49,15 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let mut out = Vec::with_capacity(numbers.len());
+    //let mut out = Vec::with_capacity(numbers.len());
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
-    for dr in division_results {
-        match dr {
-            Ok(i) => out.push(i),
-            Err(e) => return Err(e),
-        }
-    }
+    let out = division_results.collect()?;
+    // for dr in division_results {
+    //     match dr {
+    //         Ok(i) => out.push(i),
+    //         Err(e) => return Err(e),
+    //     }
+    // }
     Ok(out)
 }
 
