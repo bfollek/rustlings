10af0f02961570ccee559ffd2105482c0753063d
Bob Follek <bfollek@gmail.com>
2022-10-10 20:27:16 -0400

    Finally worked out how to use the question mark operator in an iterator.

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -49,15 +49,16 @@ pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
 // Desired output: Ok([1, 11, 1426, 3])
 fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let mut out = Vec::with_capacity(numbers.len());
+    //let mut out = Vec::with_capacity(numbers.len());
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
-    for dr in division_results {
-        match dr {
-            Ok(i) => out.push(i),
-            Err(e) => return Err(e),
-        }
-    }
-    Ok(out)
+    Ok(division_results.collect::<Result<Vec<_>, _>>())?
+    // for dr in division_results {
+    //     match dr {
+    //         Ok(i) => out.push(i),
+    //         Err(e) => return Err(e),
+    //     }
+    // }
+    // Ok(out)
 }
 
 // Complete the function and return a value of the correct type so the test passes.
