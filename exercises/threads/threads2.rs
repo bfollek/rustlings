e534261ecad6500c0a1f93cb808f0840345b84e9
Bob Follek <bfollek@gmail.com>
2022-10-13 17:30:04 -0400

    Finished thread2. This stuff is complicated.

--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -3,9 +3,7 @@
 // Building on the last exercise, we want all of the threads to complete their work but this time
 // the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -14,21 +12,33 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
-        let status_shared = status.clone();
+        let status = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut status = status.lock().unwrap();
+            status.jobs_completed += 1;
         });
         handles.push(handle);
     }
-    for handle in handles {
-        handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
-        // interesting in the output? Do you have to 'join' on all the handles?
-        println!("jobs completed {}", ???);
-    }
+
+    // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
+    // interesting in the output? Do you have to 'join' on all the handles?
+
+    // The output is `jobs completed 10` for all the threads, as if they all arrive here
+    // at the same time. So I don't have to join on all the handles. I don't have to join
+    // on _any_handle. Is the shared Arc mutex somehow responsible for this? It must be,
+    // but why? Because the Arc clones exist until all the threads are finished, and the
+    // Arc ref count is therefor > 1? I dunno.
+
+    /*
+     * Use `remove` to get ownership of the handle.
+     * https://stackoverflow.com/questions/68966949/unable-to-join-threads-from-joinhandles-stored-in-a-vector-rust
+     */
+    //let h = handles.remove(0);
+    //h.join().unwrap();
+    let status = status.lock().unwrap();
+    println!("jobs completed {}", status.jobs_completed);
 }
